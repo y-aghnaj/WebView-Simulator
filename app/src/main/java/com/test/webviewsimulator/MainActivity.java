360b262d37f0e55391c216ddfbfd3d90758676fd
y-aghnaj <yaghnaj@gmail.com>
2024-06-02 18:09:59 +0100

    Added commentary to the code

--- app/src/main/java/com/test/webviewsimulator/MainActivity.java
+++ app/src/main/java/com/test/webviewsimulator/MainActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Build;
@@ -14,6 +15,8 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -22,11 +25,11 @@ import androidx.core.app.NotificationManagerCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity"; // Tag pour les journaux
-    private WebView myWebView; // Instance de WebView
-    private static final String CHANNEL_ID = "js_alert_channel"; // ID du canal de notification
-    private static final String URL = "http://192.168.1.108:8080"; // URL statique pour charger dans WebView
-    private static final int PERMISSION_REQUEST_CODE = 1; // Code de demande de permission
+    private static final String TAG = "MainActivity";
+    private WebView myWebView;
+    private static final String CHANNEL_ID = "js_alert_channel";
+    private static final String URL = "http://test-cam.duckdns.org:8080/";
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -34,65 +37,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createNotificationChannel(); // Créer un canal de notification pour les alertes JS
+        createNotificationChannel();
 
-        myWebView = findViewById(R.id.webview); // Initialiser WebView depuis le layout
-        myWebView.setWebViewClient(new myWebClient()); // Définir un WebViewClient personnalisé
-        myWebView.setWebChromeClient(new myWebChromeClient()); // Définir un WebChromeClient personnalisé
+        myWebView = findViewById(R.id.webview);
+        myWebView.setWebViewClient(new MyWebClient());
+        myWebView.setWebChromeClient(new MyWebChromeClient());
         WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true); // Activer JavaScript
-        webSettings.setDomStorageEnabled(true); // Activer le stockage DOM
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
 
-        // Charger l'URL statique
         myWebView.loadUrl(URL);
-        Log.d(TAG, "Chargement de l'URL : " + URL);
+        Log.d(TAG, "Loading URL: " + URL);
 
-        myWebView.addJavascriptInterface(new WebAppInterface(), "Android"); // Ajouter une interface JavaScript
+        myWebView.addJavascriptInterface(new WebAppInterface(), "Android");
 
-        // Vérifier la permission de notification
         checkNotificationPermission();
+
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter("motion_motion_1");
+        MotionDetectorBroadcastReceiver receiver = new MotionDetectorBroadcastReceiver();
+        registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED);
     }
 
-    // Méthode pour vérifier et demander la permission de notification
+
     private void checkNotificationPermission() {
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, PERMISSION_REQUEST_CODE);
+        } else {
+            Log.d(TAG, "Notification permission already granted");
         }
     }
 
-    // Classe personnalisée WebViewClient pour gérer les événements de chargement de page
-    private static class myWebClient extends WebViewClient {
+    private static class MyWebClient extends WebViewClient {
         @Override
         public void onPageStarted(WebView view, String url, Bitmap favicon) {
             super.onPageStarted(view, url, favicon);
-            Log.d(TAG, "Page commencée à charger : " + url);
+            Log.d(TAG, "Page started loading: " + url);
         }
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url); // Charger la nouvelle URL
-            Log.d(TAG, "Chargement de l'URL remplacé : " + url);
+            view.loadUrl(url);
+            Log.d(TAG, "Overriding URL: " + url);
             return true;
         }
     }
 
-    // Classe personnalisée WebChromeClient pour gérer les messages de console et autres événements web
-    private class myWebChromeClient extends WebChromeClient {
+    private class MyWebChromeClient extends WebChromeClient {
         @Override
         public boolean onConsoleMessage(android.webkit.ConsoleMessage consoleMessage) {
-            Log.d(TAG, "Message de console : " + consoleMessage.message());
+            Log.d(TAG, "Console message: " + consoleMessage.message());
             String message = consoleMessage.message();
-            if (message.equals("motion motion: 1")) {
-                // Envoyer une diffusion lorsque un message de console spécifique est reçu
-                Intent intent = new Intent("motion motion: 1 with tag MainActivity");
+            if (message.contains("motion 1")) {
+                Intent intent = new Intent("motion_motion_1");
                 sendBroadcast(intent);
-                Log.d(TAG, "Diffusion envoyée pour motion: 1");
+                Log.d(TAG, "Broadcast sent for motion: 1 with message: " + message);
             }
             return super.onConsoleMessage(consoleMessage);
         }
     }
 
-    // Remplacer le comportement du bouton retour pour naviguer dans l'historique de WebView
     @Override
     public void onBackPressed() {
         if (myWebView.canGoBack()) {
@@ -102,11 +106,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Méthode pour créer un canal de notification pour les appareils exécutant Android O et supérieur
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "Canal d'alerte JavaScript";
-            String description = "Canal pour les notifications d'alerte JavaScript";
+            CharSequence name = "JavaScript Alert Channel";
+            String description = "Channel for JavaScript alert notifications";
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
@@ -116,16 +119,44 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Classe d'interface JavaScript pour permettre l'interaction depuis le JavaScript de la page web
+    public static class MotionDetectorBroadcastReceiver extends BroadcastReceiver {
+        private static final String TAG = "MotionDetectorBroadcastReceiver";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "Broadcast received for action: " + intent.getAction());
+            if ("motion_motion_1".equals(intent.getAction())) {
+                sendNotification(context);
+            }
+        }
+
+        private void sendNotification(Context context) {
+            Log.d(TAG, "Sending notification...");
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                    .setContentTitle("Motion Detected")
+                    .setContentText("A motion has been detected.")
+                    .setSmallIcon(R.drawable.notification_icon)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setAutoCancel(true);
+
+            if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "Permission not granted to post notifications");
+                return;
+            }
+
+            NotificationManagerCompat.from(context).notify(0, builder.build());
+            Log.d(TAG, "Notification sent");
+        }
+    }
+
     public class WebAppInterface {
         @JavascriptInterface
         public void log(String message) {
-            Log.d(TAG, "Message de log de l'interface JavaScript : " + message);
+            Log.d(TAG, "JavaScript log message: " + message);
             if ("motion: 1".equals(message) || "motion motion: 1".equals(message)) {
-                // Envoyer une diffusion lorsque un message spécifique est reçu du JavaScript
-                Intent intent = new Intent("motion motion: 1 with tag MainActivity");
+                Intent intent = new Intent("motion_motion_1");
                 sendBroadcast(intent);
-                Log.d(TAG, "Diffusion envoyée pour motion: 1");
+                Log.d(TAG, "Broadcast sent for motion: 1 with message: " + message);
             }
         }
     }
